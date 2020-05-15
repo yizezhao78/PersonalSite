a71ec3c62701ecbb663299e0d184a931a1ac5338
Yize Zhao <yizezhao777@gmail.com>
2020-05-14 20:20:37 -0400

    test2

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,24 @@
 import React from "react"
+import { Link } from "gatsby"
+import Header from "../components/header"
 
 export default function Home() {
     return (
       <div style={{ color: `purple` }}>
-        <h1>Hello Gatsby!</h1>
+        <Link to="/contact/">Contact</Link>
+        <Header headerText="Hello Gatsby!" />
         <p>What a world.</p>
+        {<img src="https://source.unsplash.com/random/400x200" alt="" />
+         }
+         <PrimaryButton zhao="yize">
+           s
+         </PrimaryButton>
       </div>
-    );
+    )
+  }
+
+  function PrimaryButton(props) {
+  return (<button class="primary-button">
+      {props.children}
+      </button>)
   }
